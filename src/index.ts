bd7140bbdd66ee89c257ef1b496925679bc193b8
ToniMaunde <maundemilton@gmail.com>
2023-06-12 21:52:09 +0200

    initial commit
    not really though

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+type Item = {
+	name: string;
+	type: string;
+	quantity: number;
+}
+
+// WIP
+type Condition<T, CriteriaValue> = {
+	value: keyof T;
+	criteria: CriteriaValue;
+	groups: Array<keyof T>;
+}
+
+function arrayGroup<T extends Object, KeyType extends keyof T>(collection: T[], attributeName: KeyType, condition?: Condition<T>) {
+
+	// init return object
+	const groupingObject: Record<string, T[]> = {};
+
+	if (condition !== undefined) {
+
+		return groupingObject;
+	} else {
+		const valuesForAttributeToGroupBy = collection.map(item => item[attributeName]);
+
+		for (const attributeValue of valuesForAttributeToGroupBy) {
+			const itemsGroup = collection.filter(item => item[attributeName] === attributeValue);
+
+			// convert the attributeValue to string before using it as a key
+			groupingObject[attributeValue as string] = itemsGroup;
+
+
+			return groupingObject;
+		};
+
+
+	}
+}
+
+const inventory: Item[] = [
+	{ name: "asparagus", type: "vegetables", quantity: 5 },
+	{ name: "bananas", type: "fruit", quantity: 0 },
+	{ name: "goat", type: "meat", quantity: 23 },
+	{ name: "cherries", type: "fruit", quantity: 5 },
+	{ name: "fish", type: "meat", quantity: 22 },
+];
+
+// For working with discrete values
+const groupedItems = arrayGroup(inventory, "type");
+
+console.log(groupedItems);
