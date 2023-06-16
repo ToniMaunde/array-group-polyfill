de1862593b2e0f14ee152c88e24101d2fdc20d14
ToniMaunde <maundemilton@gmail.com>
2023-06-16 16:00:14 +0200

    feat: add cache and type export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-type GroupingCondition = {
+export type GroupingCondition = {
 	value: string | number | boolean;
 	criterion: "EQUAL_STRINGS" | "STRING_INCLUDES" | "EQUAL_BOOLEANS" | "EQUAL_NUMBERS" | "GREATER_THAN" | "LESS_THAN" | "GREATER_THAN_OR_EQUAL" | "LESS_THAN_OR_EQUAL";
 	caseSensitive?: boolean;
